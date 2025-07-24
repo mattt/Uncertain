bff5da32aabea471474e8202b0ccfc7b39b98a04
Mattt Zmuda <mattt@me.com>
2025-07-24 09:44:11 -0700

    Configure swift-tools-version: 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Uncertain",
-            targets: ["Uncertain"]),
+            targets: ["Uncertain"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
